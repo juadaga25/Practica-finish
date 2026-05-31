7d6020b2a432f575d112a5118ba75a43f4386c70
Juanes <ljuanes680@gmail.com>
2026-05-31 00:22:00 -0500

    creacion del HU3

---
+++ TabletGrafica.java
@@ -0,0 +1,64 @@
+public class TabletGrafica {
+    private String Serial;
+    private String Marca;
+    private float Tamaño;
+    private float precio;
+    private int almacenamiento;
+    private float peso;
+    
+    public TabletGrafica() {
+    }
+
+    public String getSerial() {
+        return Serial;
+    }
+
+    public void setSerial(String serial) {
+        Serial = serial;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+
+    public float getTamaño() {
+        return Tamaño;
+    }
+
+    public void setTamaño(float tamaño) {
+        Tamaño = tamaño;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getAlmacenamiento() {
+        return almacenamiento;
+    }
+
+    public void setAlmacenamiento(int almacenamiento) {
+        this.almacenamiento = almacenamiento;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    
+
+    
+
+}
