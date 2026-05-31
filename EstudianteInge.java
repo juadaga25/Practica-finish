f5b199eaa88c2a50c54bb7027ba61f13f978d035
Juanes <ljuanes680@gmail.com>
2026-05-31 00:14:36 -0500

    Creacion del HU2

---
+++ EstudianteInge.java
@@ -0,0 +1,61 @@
+public class EstudianteInge {
+    private String Cedula;
+    private String Nombre;
+    private String Telefono;
+    private int NumSemestre;
+    private float PromAcumulado;
+    private String SerialEquipo;
+
+    public EstudianteInge() {
+    }
+
+    public String getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(String cedula) {
+        Cedula = cedula;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public int getNumSemestre() {
+        return NumSemestre;
+    }
+
+    public void setNumSemestre(int numSemestre) {
+        NumSemestre = numSemestre;
+    }
+
+    public float getPromAcumulado() {
+        return PromAcumulado;
+    }
+
+    public void setPromAcumulado(float promAcumulado) {
+        PromAcumulado = promAcumulado;
+    }
+
+    public String getSerialEquipo() {
+        return SerialEquipo;
+    }
+
+    public void setSerialEquipo(String serialEquipo) {
+        SerialEquipo = serialEquipo;
+    }
+
+    
+}
